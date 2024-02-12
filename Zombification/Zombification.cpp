12b0e3bfd87f1b809de9786c64ee8899930145a1
MartinsFlorian <f.martins2005.fm@gmail.com>
2024-02-12 15:24:18 +0100

    profiler correction

--- Zombification/Zombification.cpp
+++ Zombification/Zombification.cpp
@@ -5,17 +5,8 @@
 
 int main()
 {
-    srand(time(NULL));
-    int day = 1;
-    string input;
-    
-    while (day <= 28) 
-    {
-        cout << "Day :" << day << endl;
-        company.Show();
-        input = _getch();
-        day += 1;
-        system("cls");
-    }
+    unique_ptr<Zombification> zombie = make_unique<Zombification>();
+    zombie->RunDiagnostic();
 }
+    
 
--- Zombification/Zombification.h
+++ Zombification/Zombification.h
@@ -4,3 +4,24 @@
 #include "GlobalHeader.h"
 
 Company company;
+
+class Zombification 
+{
+public:
+	void RunDiagnostic() 
+	{
+        srand(time(NULL));
+        int day = 1;
+        string input;
+
+        while (day <= 28)
+        {
+            cout << "Day :" << day << endl;
+            company.Show();
+            input = _getch();
+            day += 1;
+            system("cls");
+        }
+    }
+	
+};
